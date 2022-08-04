1fea56e9ac04e438db983cafb9e2a1ef1c5f17be
Volodya <vovakry24@gmail.com>
2022-08-04 15:35:28 +0300

    1

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeaderCurrenciesLine from './HeaderCurrenciesLine'
+
+export default function Header() {
+  return (
+    <div>
+        <HeaderCurrenciesLine/>
+    </div>
+  )
+}
---
+++ src/components/header/HeaderCurrenciesLine.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function HeaderCurrenciesLine(props) {
+    
+  return (
+    <div>
+        
+    </div>
+  )
+}
